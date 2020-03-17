2d02b55b89db1c11e482587782e83186acf19fe2
AndresTapiero <andrestapiero72@gmail.com>
2020-03-16 21:37:04 -0500

    add ciclo to add ingredients

--- src/Menu.kt
+++ src/Menu.kt
@@ -1,5 +1,3 @@
-
-
 fun main(args: Array<String>) {
 
     val parrafo = """
@@ -12,55 +10,100 @@ fun main(args: Array<String>) {
 
     val recetas = listOf("Arroz Paisa, ", "Plato Mexicano, ", "Calentado, ", "Ajiaco, ", "Chiguiro")
     val currentOption = "Ha seleccionado la opción"
-        println("::Bienvenido a Recipe Maker::")
-        println(parrafo)
-        var opt: String? = readLine()
+    println("::Bienvenido a Recipe Maker::")
+    println(parrafo)
+    var opt: String? = readLine()
 
-        when (opt) {
-            "1" -> {
-                println(viewRecipe(category = "$currentOption $opt"))
-                makeRecipe()
-            }
-            "2" -> {
-                println("$currentOption: $opt")
-                println(viewRecipe(category = "$currentOption $opt"))
-                println("Sus recetas son:")
-                recetas.forEach { print(it) }
-            }
-            "3" -> println("$currentOption $opt gracias por usar Recipe maker")
-            else -> println("El valor $opt no esta en el rango")
+    when (opt) {
+        "1" -> {
+            println(viewRecipe(category = " crear recetas opción $opt"))
+            makeRecipe()
         }
+        "2" -> {
+            println("$currentOption: $opt")
+            println(viewRecipe(category = " ver mis recetas opción 2 $opt"))
+            println("Sus recetas son:")
+            recetas.forEach { print(it) }
+        }
+        "3" -> println("$currentOption $opt gracias por usar Recipe maker")
+        else -> println("El valor $opt no esta en el rango")
     }
+}
 
 
-fun makeRecipe(){
+fun makeRecipe() {
     val titleIngredients = "Los ingredientes que tenemos son: "
     println(titleIngredients)
-    val ingredientes = listOf("1) Agua, ", "2) Leche, ", "3) Carne, ", "4) Verduras, ", "5) Frutas, ", "6) Cereal, ", "7) Huevos ", "8) Aceite")
-    ingredientes.forEach{(println(it))}
-    println("Agregue sus ingredientes seleccionando el número: ")
+    val ingredientes = listOf(
+        "1) Agua, ",
+        "2) Leche, ",
+        "3) Carne, ",
+        "4) Verduras, ",
+        "5) Frutas, ",
+        "6) Cereal, ",
+        "7) Huevos ",
+        "8) Aceite"
+    )
+    ingredientes.forEach { (println(it)) }
+    println("Agregue un ingredientes seleccionando el número: ")
     addIngredients()
 }
 
-fun viewRecipe(category: String):Unit{
-    //viewRecipe() -> Mostrará el título de la categoría ingresada, así: Ver mis recetas
+fun viewRecipe(category: String): Unit {
     println("La Categoria seleccionada es : $category")
 
 }
 
 fun addIngredients() {
-    loop@ do {
-        val ingrediente = """
-        """.trimIndent()
-        println(ingrediente)
-        val ingre: Int = readLine()?.toInt() as Int
-        when(ingre){
-            1-> {print("Agua Agregada")
-                break@loop
+    do {
+        println("Seleccione el # del ingrediente a agregar")
+        loop@ do {
+            val ingre: Int = readLine()?.toInt() as Int
+            when (ingre) {
+                1 -> {
+                    println("Agua Agregada")
+                    break@loop
+                }
+                2 -> {
+                    println("Leche Agregada")
+                    break@loop
+                }
+                3 -> {
+                    println("Carne Agregada")
+                    break@loop
+                }
+                4 -> {
+                    println("Verduras Agregada")
+                    break@loop
+                }
+                5 -> {
+                    println("Frutas Agregada")
+                    break@loop
+                }
+                6 -> {
+                    println("Cereal Agregada")
+                    break@loop
+                }
+                7 -> {
+                    println("Huevos Agregada")
+                    break@loop
+                }
+                8 -> {
+                    println("Aceite Agregada")
+                    break@loop
+                }
+                else -> println("Valor no encontrado")
             }
-            else -> println("Valor no encontrado")
-        }
-    }while (ingre!=9)
+        } while (break)
+        println(
+            """
+            Presione: 
+                1) para agregar otro ingrediente
+                2) para salir
+        """.trimIndent()
+        )
+        val input = readLine()?.toInt() as Int
+    } while (input == 1)
 }
 
 
